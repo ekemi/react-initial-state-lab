ace6fc6c8db52e1c99144c801cf9e0aaa21e4af3
Yapnounkie <edmundopierre@yahoo.com>
2021-02-01 18:43:53 -0800

    add the missing part of the code

--- src/Bomb.js
+++ src/Bomb.js
@@ -1,1 +1,32 @@
 // your Bomb code here!
+import React, { Component } from 'react'
+
+class Bomb extends Component {
+
+    constructor (props) {
+        super()
+        this.state = {
+            secondsLeft: props.initialCount
+
+        }
+    }
+
+    bom () {
+        if(this.state.secondsLeft ===120){
+         return   <h6>{this.state.secondsLeft}seconds left before I go boom!</h6>
+
+        }
+        else {
+            if(this.state.secondsLeft===0){
+               return <h6>Bom!</h6>
+            }
+        }
+    }
+    render() {
+        return(
+            <h6>{this.bom()}</h6>
+        )
+
+    }
+}
+export default Bomb
--- src/ImageSlider.js
+++ src/ImageSlider.js
@@ -1,1 +1,17 @@
 // your ImageSlider code here!
+ import React, { Component } from 'react'
+
+
+
+ class ImageSlider extends Component {
+    constructor() {
+        super()
+        this.state ={
+           currentSlideIndex : 0
+        }
+    }
+     render() {
+         return(<h6>{this.state.currentSlideIndex}</h6>)
+     }
+ }
+ export default ImageSlider
